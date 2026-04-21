db522f887487c64a6ab89b3aabfaa8de128f8454
Munazar <munazar.ali.001@gmail.com>
2026-04-21 21:41:11 +0100

    feat: creates server function for sending the invite email

---
+++ features/users/server/email/sendInvitationEmail.ts
@@ -0,0 +1,27 @@
+import InvitationEmail from '@/components/email/Invite';
+import { resend } from '@/lib/resend/resend';
+
+export const sendInvitationEmail = async (email: string, name: string) => {
+  const { error } = await resend.emails.send({
+    from: 'support@munazar-ali.dev',
+    to: [email],
+    subject: 'Welcome Aboard! Set Up Your Account',
+    react: InvitationEmail({
+      name,
+      email,
+    }),
+  });
+
+  if (error) {
+    console.error({ error });
+    return {
+      success: false,
+      message: `Failed to send email: ${error.message}`,
+    };
+  }
+
+  return {
+    success: true,
+    message: `An email has been sent to ${email}.`,
+  };
+};
