2021495f058cf3d27cdf5527b51f5a4fd3c9cf88
Munazar <munazar.ali.001@gmail.com>
2026-04-22 22:49:10 +0100

    feat: implents new user invite email being sendt
    implents new user invite email being sent

--- features/users/server/actions/create-user.action.ts
+++ features/users/server/actions/create-user.action.ts
@@ -3,6 +3,10 @@
 import prisma from '@/lib/prisma/prisma';
 import { encrypt } from '../../utils/crypto';
 import { AddUserFormValues, addUserSchema } from '../../utils/schema';
+import { sendInvitationEmail } from '../email/sendInvitationEmail';
+import { invalidateUserPasswordResetSessions } from '@/server/db/auth/invalidateUserPasswordResetSessions';
+import { generateSessionToken } from '@/utils/auth/generateSessionToken';
+import { createPasswordResetSession } from '@/server/db/auth/createPasswordResetSession';
 
 export async function createUserAction(formData: AddUserFormValues) {
   const parsedData = addUserSchema.safeParse(formData);
@@ -37,8 +41,19 @@ export async function createUserAction(formData: AddUserFormValues) {
         status: 'pending',
       },
     });
-
     if (sendInvite) {
+      await invalidateUserPasswordResetSessions(newUser.id);
+
+      // Generate and store a new password reset token
+      const token = generateSessionToken();
+      await createPasswordResetSession(token, newUser.id, newUser.email);
+      const sendInvitation = await sendInvitationEmail(email, name, token);
+      if (!sendInvitation.success) {
+        console.error(
+          `Failed to send invitation email: ${sendInvitation.message}`,
+        );
+      }
+
       // TODO: Send an email with a signup link (e.g., `/complete-signup?token=xyz`)
     }
 
