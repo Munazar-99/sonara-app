252dad061018c82efb7b30bd962dbdf028b3d82c
Munazar <munazar.ali.001@gmail.com>
2026-05-11 23:25:19 +0100

    style: adjust width of request reset

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -3,21 +3,22 @@ import { GalleryVerticalEnd } from 'lucide-react';
 import React from 'react';
 
 const page = () => {
-  return (<div className="flex min-h-[80vh] flex-col gap-0 p-6 md:p-10 bg-white">
-  <div className="flex justify-center gap-2 md:justify-start">
-    <a href="#" className="flex items-center gap-2 font-medium text-dark">
-      <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-white">
-        <GalleryVerticalEnd className="size-4" />
+  return (
+    <div className="flex flex-1 flex-col gap-0 bg-white p-6 md:p-10">
+      <div className="flex justify-center gap-2 md:justify-start">
+        <a href="#" className="flex items-center gap-2 font-medium text-dark">
+          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-white">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          Acme Inc.
+        </a>
       </div>
-      Acme Inc.
-    </a>
-  </div>
-  <div className="flex flex-1 items-center justify-center">
-    <div className="w-full">
-    <RequestReset />
+      <div className="flex flex-1 items-center justify-center">
+        <div className="w-full">
+          <RequestReset />
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 };
 
