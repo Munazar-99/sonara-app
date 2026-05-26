786bb96a5230b3d4a3601b83f9f40fdc0df8cc53
Munazar <munazar.ali.001@gmail.com>
2026-05-26 09:38:24 +0100

    refactor: implements dynamic invite/reset links

---
+++ utils/url/getAppUrl.ts
@@ -0,0 +1,9 @@
+export function getAppUrl() {
+  return (
+    process.env.NEXT_PUBLIC_APP_URL ??
+    process.env.APP_URL ??
+    (process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : 'http://localhost:3000')
+  ).replace(/\/$/, '');
+}
